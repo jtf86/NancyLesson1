985998b0dc13a32b5d9a78370dba3272894bf551
jtf86 <john@epicodus.com>
2016-01-12 10:23:59 -0800

    complete initial lessons

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -7,12 +7,21 @@ namespace FriendLetter
   {
     public HomeModule()
     {
+      Get["/form"] = _ => View["form.html"];
       Get["/"] = _ => {
         var myLetterVariables = new LetterVariables {
           Recipient = "Jill",
           Sender = "John"
         };
         return View["hello.html", myLetterVariables];
-      };    }
+      };
+      Get["/greeting_card"] = _ => {
+        var myLetterVariables = new LetterVariables {
+          Sender = Request.Query["sender"],
+          Recipient = Request.Query["recipient"]
+        };
+        return View["hello.html", myLetterVariables];
+      };
+    }
   }
 }
